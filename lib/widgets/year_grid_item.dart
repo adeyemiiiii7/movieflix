4c38a4d23c0df30818d34ba29fba954076f62029
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-07-12 02:00:56 -0400

    new file:   lib/widgets/year_grid_item.dart

---
+++ lib/widgets/year_grid_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:movieflix/models/year.dart';
+
+class MovieGridItem extends StatelessWidget {
+  const MovieGridItem({
+    super.key,
+    required this.year,
+  });
+
+  final Year year;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
