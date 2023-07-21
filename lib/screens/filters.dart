085c4a42e9bc8c27cb62a15115d4b36a4f592d86
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2023-07-21 18:33:34 -0400

    still filter options

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -57,6 +57,86 @@ class _FiltersScreenState extends State<FiltersScreen> {
           activeColor: Theme.of(context).colorScheme.tertiary,
           contentPadding: const EdgeInsets.only(left: 34, right: 22),
         ),
+        SwitchListTile(
+          value: _comedyMovies,
+          onChanged: (isChecked) {
+            setState(() {
+              _comedyMovies = isChecked;
+            });
+          },
+          title: Text(
+            'Comedy-Movies',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _animationMovies,
+          onChanged: (isChecked) {
+            setState(() {
+              _animationMovies = isChecked;
+            });
+          },
+          title: Text(
+            'Animation-Movies',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _horrorMovies,
+          onChanged: (isChecked) {
+            setState(() {
+              _horrorMovies = isChecked;
+            });
+          },
+          title: Text(
+            'Horror-Movies',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _horrorMovies,
+          onChanged: (isChecked) {
+            setState(() {
+              _horrorMovies = isChecked;
+            });
+          },
+          title: Text(
+            'Horror-Movies',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
+        SwitchListTile(
+          value: _thrillerMovies,
+          onChanged: (isChecked) {
+            setState(() {
+              _thrillerMovies = isChecked;
+            });
+          },
+          title: Text(
+            'Thriller-Movies',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          activeColor: Theme.of(context).colorScheme.tertiary,
+          contentPadding: const EdgeInsets.only(left: 34, right: 22),
+        ),
       ]),
     );
   }
